02240cb7650ae72606b18fc6c0a3e9300beb8586
PhilGreen-Dev <phillip.g.green@outlook.com>
2024-08-27 00:07:44 -0400

    Update CSS for mobile view and add hamburger menu functionality

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 		});
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+	const hamburger = document.getElementById('hamburger');
+	const navMenu = document.getElementById('nav-menu');
+
+	hamburger.addEventListener('click', function () {
+		navMenu.classList.toggle('active');
+		hamburger.classList.toggle('active');
+	});
+});
